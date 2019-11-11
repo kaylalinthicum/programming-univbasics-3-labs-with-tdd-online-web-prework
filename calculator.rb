4b93f9be9131bb0e290f60c073626e92f4f1a1ea
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-11 01:05:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 first_number = 12
 second_number = 10 
 
-def sum(first_number + second_number)
-end
-
-def
+sum = (first_number + second_number)
+difference = (first_number - second_number)
+product = (first_number * second_number)
+quotient = (first_number / second_number)
